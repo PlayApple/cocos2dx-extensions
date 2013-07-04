a9ffb0bf8233b03eb92eba877fe0d19648b51862
Sunny <cocos2der@gmail.com>
2013-07-04 18:26:21 +0800

    add test

---
+++ Cocos2dxExt/Cocos2dxExt/Classes/EXMainMenuScene.cpp
@@ -0,0 +1,9 @@
+//
+//  EXMainMenuScene.cpp
+//  Cocos2dxExt
+//
+//  Created by LiuYanghui on 13-7-4.
+//
+//
+
+#include "EXMainMenuScene.h"
---
+++ Cocos2dxExt/Cocos2dxExt/Classes/EXMainMenuScene.h
@@ -0,0 +1,14 @@
+//
+//  EXMainMenuScene.h
+//  Cocos2dxExt
+//
+//  Created by LiuYanghui on 13-7-4.
+//
+//
+
+#ifndef __Cocos2dxExt__EXMainMenuScene__
+#define __Cocos2dxExt__EXMainMenuScene__
+
+#include <iostream>
+
+#endif /* defined(__Cocos2dxExt__EXMainMenuScene__) */
